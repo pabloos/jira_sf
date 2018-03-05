a6da0833489cc02f12ea396cf6e03c7d75a75f6c
PragmaOnce <pcumpian1@gmail.com>
2018-03-06 00:33:31 +0100

    removed dumb clousure

--- Jira_Show&Hide-FP.js
+++ Jira_Show&Hide-FP.js
@@ -8,20 +8,16 @@
             const show = "block" //display states
             const hide = "none"
 
-            var checks_array     = ["customfield_11278-1", "customfield_11278-2", "customfield_11278-3", "customfield_11278-4", "customfield_11278-5"].map(id => document.getElementById(id))
-            let dtp_fields       = ["customfield_11101", "customfield_11366", "customfield_11339"].map(id => document.getElementById(id).parentElement)
-                icr_req_fields   = ["customfield_11725"].map(id => document.getElementById(id).parentElement), //yep, if they want to add more fields in this section we only need to put them into the array
-                plt_fields       = ["customfield_11359", "customfield_11347", "customfield_11354", "customfield_11356", "customfield_11369"].map(id => document.getElementById(id).parentElement),
-                preflight_fields = [preflight_scope_field = document.getElementById("customfield_11371-1").parentElement.parentElement, 
-                                    preflight_notes_field = document.getElementById("customfield_11397").parentElement],
-                final_eye_fields = ["customfield_11943"].map(id => document.getElementById(id).parentElement);
+            const checks_array      = ["customfield_11278-1", "customfield_11278-2", "customfield_11278-3", "customfield_11278-4", "customfield_11278-5"].map(id => document.getElementById(id))
+            const dtp_fields        = ["customfield_11101", "customfield_11366", "customfield_11339"].map(id => document.getElementById(id).parentElement)
+                  icr_req_fields    = ["customfield_11725"].map(id => document.getElementById(id).parentElement), //yep, if they want to add more fields in this section we only need to put them into the array
+                  plt_fields        = ["customfield_11359", "customfield_11347", "customfield_11354", "customfield_11356", "customfield_11369"].map(id => document.getElementById(id).parentElement),
+                  preflight_fields  = [preflight_scope_field = document.getElementById("customfield_11371-1").parentElement.parentElement, 
+                                       preflight_notes_field = document.getElementById("customfield_11397").parentElement],
+                  final_eye_fields  = ["customfield_11943"].map(id => document.getElementById(id).parentElement);
 
             [dtp_fields, icr_req_fields, plt_fields, preflight_fields, final_eye_fields].map((section, index) => (function() {
-                section.forEach(field => {
-                    field.style.display = (function() {
-                        return (checks_array[index].checked ? show : hide)
-                    })() //we need to call the function. Otherwise this returns a pointer
-                })
+                section.forEach(field => field.style.display = (checks_array[index].checked ? show : hide))
             })).forEach((func, index) => (function() {
                 checks_array[index].addEventListener("click", func)
                 func()  //we call the functions in order to keep consistency from the begining between the ckeckboxes and the fields
